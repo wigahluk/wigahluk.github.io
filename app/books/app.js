356d207f6655c625fbd440fc700822243306d2e8
Oscar Ponce <wigahluk@gmail.com>
2015-05-25 17:33:37 -0700

    Updating books and styles.

--- app/books/app.js
+++ app/books/app.js
@@ -51,28 +51,28 @@
                 },
                 {
                     title: 'Patterns of Enterprise Application Architecture',
-                    cover: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0321127420&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=wigahlukblog-20',
+                    cover: 'https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0321127420&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=wigahlukblog-20',
                     amazonLink: 'http://www.amazon.com/gp/product/0321127420/ref=as_li_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=0321127420&linkCode=as2&tag=wigahlukblog-20&linkId=DI3TRLYV2IOSIUGA',
                     author: 'Martin Fowler',
                     stars: 5
                 },
                 {
                     title: 'Refactoring: Improving the Design of Existing Code',
-                    cover: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0201485672&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=wigahlukblog-20',
+                    cover: 'https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0201485672&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=wigahlukblog-20',
                     amazonLink: 'http://www.amazon.com/gp/product/0201485672/ref=as_li_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=0201485672&linkCode=as2&tag=wigahlukblog-20&linkId=VDAHT5G6LVUUBF2D',
                     author: 'Martin Fowler',
                     stars: 5
                 },
                 {
                     title: 'Implementation Patterns',
-                    cover: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0321413091&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=wigahlukblog-20',
+                    cover: 'https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0321413091&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=wigahlukblog-20',
                     amazonLink: 'http://www.amazon.com/gp/product/0321413091/ref=as_li_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=0321413091&linkCode=as2&tag=wigahlukblog-20&linkId=5GNPWUXBBAP3RX7A',
                     author: 'Kent Beck',
                     stars: 5
                 },
                 {
                     title: 'Functional Programming in Scala',
-                    cover: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1617290653&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=wigahlukblog-20',
+                    cover: 'https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1617290653&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=wigahlukblog-20',
                     amazonLink: 'http://www.amazon.com/gp/product/1617290653/ref=as_li_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=1617290653&linkCode=as2&tag=wigahlukblog-20&linkId=FV6M6R2W76MC2RBW',
                     author: 'Paul Chiusano, Rúnar Bjarnason',
                     stars: 5
