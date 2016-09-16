b50e4025c3cc040b46d205f6d4bb6a915d02bb3a
Oscar Ponce <wigahluk@gmail.com>
2016-09-16 12:58:33 -0700

    build is now generated with NODE_ENV=production.

--- webpack/build.conf.js
+++ webpack/build.conf.js
@@ -1,8 +1,11 @@
 /* global require */
 // Import base conf and modify it to fit the build process.
 
+const webpack = require('webpack');
 const conf = require('./base.conf');
 
 conf.devtool = 'source-map';
+
+conf.plugins.push(new webpack.DefinePlugin({ "process.env": { NODE_ENV: JSON.stringify("production") } }));
 
 module.exports = conf;
