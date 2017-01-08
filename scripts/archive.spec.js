6d8bae0c54b4830e9acc49499159ba88dc87c7d2
Oscar <wigahluke@gmail.com>
2017-01-07 17:29:47 -0800

    Updating all dependencies except Webpack.

--- scripts/archive.spec.js
+++ scripts/archive.spec.js
@@ -4,7 +4,6 @@ describe('Archive', () => {
     it('All Posts', done => {
         archive.all().filter(p => p.fileName === 'p1').subscribe(
             p => {
-                console.log(p);
                 expect(p.fileName).toBe('p1');
                 done();
             }
